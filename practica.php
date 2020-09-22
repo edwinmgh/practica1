b4e5f8e748386f217d30653bbee3a4807764aa3e
Edwin Gallegos <edwingallegos.06032001@gmail.com>
2020-09-22 11:00:39 -0600

    version 1

---
+++ practica.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        html 
+        {
+            width: 100%;
+            height: 100%;
+        }
+
+        body 
+        {
+            width: 100%;
+            height: 100%;
+        }
+        header{
+            width: 100%;
+            height: 20%;
+            background-color: blue;
+        }
+        nav{
+            width: 100%;
+            height: 10%;
+            background-color: indigo;
+        }
+        main{
+            width: 100%;
+            height: 60%;
+            background-color: grey;
+        }
+        aside{
+            width: 20%;
+            height: 100%;
+            background-color: pink;
+            float: left;
+        }
+        article{
+            width: 80%;
+            height: 100%;
+            background-color: green;
+            float: left;
+        }
+        footer{
+            width: 100%;
+            height: 10%;
+            background-color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <header></header>
+    <nav></nav>
+    <main>
+        <aside></aside>
+        <article></article>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
